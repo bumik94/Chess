f84fada3b5d8472202e004be6ea898d2bbce5103
Cypeace <vaclav.himl@gmail.com>
2024-08-26 22:10:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+
+public class Main {
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Chess");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new ConnectTheDots());
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+}
